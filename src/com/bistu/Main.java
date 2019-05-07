e79cb970ff368ceece90757794519ca246f07cde
chensj <chensj1996exo@163.com>
2019-05-07 17:06:07 +0800

    hello1

---
+++ src/com/bistu/Main.java
@@ -0,0 +1,10 @@
+package com.bistu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+    }
+}
